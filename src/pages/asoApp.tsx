58885c36cc8b8bd3597a997b24ebf066e46ea285
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2025-12-23 17:07:08 +0000

    Agregar componente ASOapp y ruta correspondiente en App

---
+++ src/pages/asoApp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export const ASOapp: React.FC = () => {
+  return (
+    <main>
+      <h1>ASO.app</h1>
+      <p>La ASO.app se está reescribiendo.</p>
+    </main>
+  );
+}
